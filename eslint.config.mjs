bbfc11b50e54835487015f71e0bf881985db5ee9
habib ulla <habibulla1278@gmail.com>
2024-11-28 08:48:34 +0600

    first commit

---
+++ eslint.config.mjs
@@ -0,0 +1,27 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    rules: {
+      eqeqeq: "off",
+      "no-unused-vars": "error",
+      "no-console": "warn",
+      "no-unused-expressions": "error",
+      "no-undefined": "error",
+      "prefer-const": ["error", { ignoreReadBeforeAssign: true }],
+    },
+  },
+
+  {
+    ignores: [".node_modules/*", "dist/*"]
+  }
+  ,
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+];

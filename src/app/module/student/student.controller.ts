835c482fbf09d4528f5c5ac1e17ce6b310af7514
habib ulla <habibulla1278@gmail.com>
2024-11-29 09:05:34 +0600

    get method - single  student data

--- src/app/module/student/student.controller.ts
+++ src/app/module/student/student.controller.ts
@@ -35,7 +35,37 @@ const getAllStudent = async (req: Request, res: Response) => {
   }
 };
 
+export const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const id = req.params.studentId;
+    const result = await studentServices.getSingleStudentDataFromDB(id);
+
+    if (!result) {
+      res.status(404).json({
+        success: false,
+        message: `No student found with the ID: ${id}`,
+      });
+      return;
+    }
+
+    res.status(200).json({
+      success: true,
+      message: `Successfully retrieved data for student with ID: ${id}`,
+      data: result,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: 'An error occurred while retrieving the student data.',
+      error: (error as Error).message,
+    });
+  }
+};
+
+
+
 export const studentControllers = {
   createStudent,
-  getAllStudent
+  getAllStudent,
+  getSingleStudent,
 };
--- src/app/module/student/student.route.ts
+++ src/app/module/student/student.route.ts
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.post('/create-student', studentControllers.createStudent);
 router.get('/', studentControllers.getAllStudent);
+router.get('/:studentId', studentControllers.getSingleStudent);
 
 export const studentRoutes = router;
